50ccf45d2901d60acc2c55c828ee4d7ba0ec634f
hvalle <hugovalle1@weber.edu>
2017-09-26 13:03:05 -0600

    Initial commit

---
+++ task3.cpp
@@ -0,0 +1,16 @@
+// File: task3.cpp
+// Created by Hugo Valle on 9/26/2017 for CS1410.
+// Copyright (c) 2017 WSU
+#include <iostream>
+
+using namespace std;
+// Constants, Structs, Classes
+
+// Prototypes
+
+// Main Program Program
+int main() {
+
+    return 0;
+}
+// Function Definitions
