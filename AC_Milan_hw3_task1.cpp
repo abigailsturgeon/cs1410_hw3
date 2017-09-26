007830a0b02186967fccc181a3e5f8caf85226af
kevtenney <kevintenney@mail.weber.edu>
2017-09-26 13:49:05 -0600

    Merge branch 'master' of C:\Users\Owner\CS1410\cs1410_hw3clone with conflicts.

--- AC_Milan_hw3_task1.cpp
+++ AC_Milan_hw3_task1.cpp
@@ -3,18 +3,6 @@ using namespace std;
 
 int main()
 {
-
-    class date
-    {
-    private:
-        int month, day, year;
-    public:
-        void getDate()
-        {
-            int tempchar;
-            cout << "Enter a date (format 12/31/99): " << endl;
-            cin << month << tempchar << day <<
-        }
-    };
+    cout<<"This is a change";
     return 0;
 }
