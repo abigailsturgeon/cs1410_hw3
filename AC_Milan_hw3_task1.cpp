f19ba0bb9f6fa8342cb28164f694ff1c5c3f38f5
abigailsturgeon <31452283+abigailsturgeon@users.noreply.github.com>
2017-09-26 13:16:46 -0600

    First draft

--- AC_Milan_hw3_task1.cpp
+++ AC_Milan_hw3_task1.cpp
@@ -3,6 +3,18 @@ using namespace std;
 
 int main()
 {
-    cout<<"This is a change";
+
+    class date
+    {
+    private:
+        int month, day, year;
+    public:
+        void getDate()
+        {
+            int tempchar;
+            cout << "Enter a date (format 12/31/99): " << endl;
+            cin << month << tempchar << day <<
+        }
+    };
     return 0;
 }
