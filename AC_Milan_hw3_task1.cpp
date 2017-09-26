b81a247d0073538b4bb6b751ba8170d869dea548
abigailsturgeon <31452283+abigailsturgeon@users.noreply.github.com>
2017-09-26 13:47:54 -0600

    Functioning task 1 without comments

--- AC_Milan_hw3_task1.cpp
+++ AC_Milan_hw3_task1.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 using namespace std;
 
+class Date
+{
+private:
+    int month, day, year;
+public:
+    void setDate(int m, int d, int y)
+    {
+        int month = m;
+        int day = d;
+       int year = y;
+    }
+    void getDate()
+    {
+        char tempchar;
+        cout << "\nEnter a date (format 12/31/99): " << endl;
+        cin >> month >> tempchar >> day >> tempchar >> year;
+    }
+    void showDate()
+    {
+       cout << month << "/" << day << "/" << year;
+    }
+};
+
 int main()
 {
+    Date d1, d2;
+    d1.setDate(0, 0, 0);
+    d1.getDate();
+    cout << "Date 1: ";
+    d1.showDate();
 
-    class date
-    {
-    private:
-        int month, day, year;
-    public:
-        void getDate()
-        {
-            int tempchar;
-            cout << "Enter a date (format 12/31/99): " << endl;
-            cin << month << tempchar << day <<
-        }
-    };
+    d2.setDate(0, 0, 0);
+    d2.getDate();
+    cout << "Date 2: ";
+    d2.showDate();
     return 0;
 }
