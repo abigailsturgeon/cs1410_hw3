63ee605aae07d00710d0d4f329e453d7f88f1dc1
kevtenney <kevintenney@mail.weber.edu>
2017-09-28 13:48:17 -0600

    Updated with comments.

--- AC_Milan_hw3_task1.cpp
+++ AC_Milan_hw3_task1.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 using namespace std;
 
+//Define class date
 class Date
 {
+//Set variables
 private:
     int month, day, year;
 public:
@@ -12,30 +14,35 @@ public:
         int day = d;
        int year = y;
     }
+    //Get user input
     void getDate()
     {
         char tempchar;
         cin >> month >> tempchar >> day >> tempchar >> year;
     }
+    //Display Information
     void showDate()
     {
        cout << month << "/" << day << "/" << year;
     }
 };
-
+//Main program
 int main()
 {
+    //Create two instances of the class
     Date d1, d2;
 
+    //set, get, first date
     cout<<"Enter a date (format 12/31/2007):"<<endl;
     d1.setDate(0, 0, 0);
     d1.getDate();
 
-
+    //set, get second date
     cout<<"Enter another date:"<<endl;
     d2.setDate(0, 0, 0);
     d2.getDate();
 
+    //Show dates
     cout << "Date 1: ";
     d1.showDate();
     cout << "\nDate 2: ";
